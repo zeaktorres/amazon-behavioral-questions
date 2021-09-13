9da3331ebd288e84a6f78ead784346a94e1df4be
Zeak <zeak@Zeaks-MacBook-Air.local>
2021-09-12 18:05:19 -0700

    Updated the behavioral questions. Finished App.js with getNewQuestion and getRandomInt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import  common_json from './Amazon-Behavioral-Questions.json'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isShown, show] = useState(false)
+  const [questionObject, updateQuestion] = useState({})
+
+  var getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  }
+
+  var getNewQuestion = () => {
+    var lp_number = getRandomInt(common_json.behavioral_questions.length)
+    var lp = common_json.behavioral_questions[lp_number].LP
+    var q = common_json.behavioral_questions[lp_number].QArray[getRandomInt(common_json.behavioral_questions[lp_number].QArray.length)]
+    return {"LP": lp, "Q": q}
+  }
+
+  useEffect(() => {
+    updateQuestion(getNewQuestion())
+  
+    // ES LINT ERROR, React Hook useEffect has a missing dependency: 'getNewQuestion'
+    // If I add the dependency, it will cause a stack overflow
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        Hello world!
+        <div className={isShown  ? "LPContainerShown" : "LPContainerHidden"}>
+          {questionObject.LP}
+        </div>
+        <div className="QContainer">
+          {questionObject.Q}
+        </div>
+        <div className="newButtonContainer">
+          <button className="revalButton" onClick={show}>Reval LP</button>
+          <button className="newButton" onClick={() => {show(false); updateQuestion(getNewQuestion())}}>New</button>
+        </div>
       </header>
     </div>
   );
